189661de1f988c29a692ffe3ed5194948adc17d9
Majkel Dudyselo <dudys.michal@gmail.com>
2018-03-13 12:30:53 +0100

    ROT13
    ROT13 ("rotate by 13 places", sometimes hyphenated ROT-13) is a simple letter substitution cipher that replaces a letter with the 13th letter after it, in the alphabet. ROT13 is a special case of the Caesar cipher, developed in ancient Rome.
    
    Because there are 26 letters (2×13) in the basic Latin alphabet, ROT13 is its own inverse; that is, to undo ROT13, the same algorithm is applied, so the same action can be used for encoding and decoding. The algorithm provides virtually no cryptographic security, and is often cited as a canonical example of weak encryption.
    
    Example:
    https://en.wikipedia.org/wiki/File:ROT13_table_with_example.svg

---
+++ ROT13.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace ROT13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Information; int x = 0;
+            Console.Title = "ROT13";
+            do
+            {
+                Console.Clear();
+                Console.Write("[1] Decode\n[2] Encode\n\nChoice: ");
+                bool Result = uint.TryParse(Console.ReadLine(), out uint Num);
+                Console.Clear();
+                if (Result)
+                {
+                    switch (Num)
+                    {
+                        case 1:
+                            Console.WriteLine("Decode: ");
+                            Information = Console.ReadLine();
+                            Console.Clear();
+                            Console.WriteLine("Information decoded from ROT13:\n");
+                            Console.WriteLine(Decode(Information));
+                            x = 1;
+                            break;
+                        case 2:
+                            Console.WriteLine("Encode: ");
+                            Information = Console.ReadLine();
+                            Console.Clear();
+                            Console.WriteLine("Information encoded in ROT13:\n");
+                            Console.WriteLine(Encode(Information));
+                            x = 1;
+                            break;
+                        default: Console.WriteLine("Wrong choice"); break;
+                    }
+                }
+                else { Console.WriteLine("Wrong choice\n\nPress any key to start again..."); Console.ReadKey(); }
+            } while (x == 0);
+        }
+        public static char[] Decode(string Cipher)
+        {
+            char[] Decoded = new char[Cipher.Length];
+            for (int i = 0; i < Cipher.Length; i++)
+            {
+                int Value = (int)Cipher[i];
+                if ((Value >= 78 && Value <= 90) || (Value >= 110 && Value <= 122)) { Value -= 13; Decoded[i] = (char)Value; }
+                else if ((Value >= 65 && Value <= 77) || (Value >= 97 && Value <= 109)) { Value += 13; Decoded[i] = (char)Value; }
+                else { Decoded[i] = (char)Value; }
+            }
+            return Decoded;
+        }
+        public static char[] Encode(string Cipher)
+        {
+            char[] Encoded = new char[Cipher.Length];
+            for (int i = 0; i < Cipher.Length; i++)
+            {
+                int Value = (int)Cipher[i];
+                if ((Value >= 65 && Value <= 77) || (Value >= 97 && Value <= 109)) { Value += 13; Encoded[i] = (char)Value; }
+                else if ((Value >= 78 && Value <= 90) || (Value >= 110 && Value <= 122)) { Value -= 13; Encoded[i] = (char)Value; }
+                else { Encoded[i] = (char)Value; }
+            }
+            return Encoded;
+        }
+    }
+}
