3f477d4cbf10d4d1e8b043566f1e262f71f81423
Majkel Dudyselo <dudys.michal@gmail.com>
2018-03-13 12:25:11 +0100

    Caesar Cipher
    In cryptography, a Caesar cipher, also known as Caesar's cipher, the shift cipher, Caesar's code or Caesar shift, is one of the simplest and most widely known encryption techniques. It is a type of substitution cipher in which each letter in the plaintext is replaced by a letter some fixed number of positions down the alphabet. For example, with a left shift of 3, D would be replaced by A, E would become B, and so on. The method is named after Julius Caesar, who used it in his private correspondence.
    
    Example:
    https://en.wikipedia.org/wiki/Caesar_cipher#/media/File:Caesar_cipher_left_shift_of_3.svg

--- ROT13.cs
+++ ROT13.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 
-namespace CaesarCipher
+namespace ROT13
 {
     class Program
     {
         static void Main(string[] args)
         {
             string Information; int x = 0;
-            Console.Title = "Caesar Cipher";
+            Console.Title = "ROT13";
             do
             {
+                Console.Clear();
                 Console.Write("[1] Decode\n[2] Encode\n\nChoice: ");
                 bool Result = uint.TryParse(Console.ReadLine(), out uint Num);
                 Console.Clear();
@@ -21,15 +22,17 @@ namespace CaesarCipher
                             Console.WriteLine("Decode: ");
                             Information = Console.ReadLine();
                             Console.Clear();
-                            Console.WriteLine("Information decoded from Caesar Cipher:\n");
+                            Console.WriteLine("Information decoded from ROT13:\n");
                             Console.WriteLine(Decode(Information));
+                            x = 1;
                             break;
                         case 2:
                             Console.WriteLine("Encode: ");
                             Information = Console.ReadLine();
                             Console.Clear();
-                            Console.WriteLine("Information encoded in Caesar Cipher:\n");
+                            Console.WriteLine("Information encoded in ROT13:\n");
                             Console.WriteLine(Encode(Information));
+                            x = 1;
                             break;
                         default: Console.WriteLine("Wrong choice"); break;
                     }
@@ -39,12 +42,12 @@ namespace CaesarCipher
         }
         public static char[] Decode(string Cipher)
         {
-               char[] Decoded = new char[Cipher.Length];
+            char[] Decoded = new char[Cipher.Length];
             for (int i = 0; i < Cipher.Length; i++)
             {
                 int Value = (int)Cipher[i];
-                if ((Value >= 68 && Value <= 90) || (Value >= 100 && Value <= 122)) { Value -= 3; Decoded[i] = (char)Value; }
-                else if ((Value >= 65 && Value <= 67) || (Value >= 97 && Value <= 99)) { Value += 23; Decoded[i] = (char)Value; }
+                if ((Value >= 78 && Value <= 90) || (Value >= 110 && Value <= 122)) { Value -= 13; Decoded[i] = (char)Value; }
+                else if ((Value >= 65 && Value <= 77) || (Value >= 97 && Value <= 109)) { Value += 13; Decoded[i] = (char)Value; }
                 else { Decoded[i] = (char)Value; }
             }
             return Decoded;
@@ -55,8 +58,8 @@ namespace CaesarCipher
             for (int i = 0; i < Cipher.Length; i++)
             {
                 int Value = (int)Cipher[i];
-                if ((Value >= 65 && Value <= 87) || (Value >= 97 && Value <= 119)) { Value += 3; Encoded[i] = (char)Value;}
-                else if((Value >= 88 && Value <= 90) || (Value >= 120 && Value <= 122)) { Value -= 23; Encoded[i] = (char)Value;}
+                if ((Value >= 65 && Value <= 77) || (Value >= 97 && Value <= 109)) { Value += 13; Encoded[i] = (char)Value; }
+                else if ((Value >= 78 && Value <= 90) || (Value >= 110 && Value <= 122)) { Value -= 13; Encoded[i] = (char)Value; }
                 else { Encoded[i] = (char)Value; }
             }
             return Encoded;
